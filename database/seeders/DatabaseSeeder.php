a891a6c3a1e99a2be49f280a122364ba5281c500
agathaliaaa <agathacyla21@gmail.com>
2025-11-26 20:23:29 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            "name" => "user",
+            "email" => "user@gmail.com",
+            "usertype" => "user",
+            "password" => "user123"
+        ]);
+
+        User::create([
+            "name" => "admin",
+            "email" => "admin@gmail.com",
+            "usertype" => "admin",
+            "password" => "admin123"
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+Route::middleware(['auth', 'userMiddleware'])->group(function () {
+    Route::get('dashboard', [UserController::class, 'index'])->name('dashboard');
+});
+
+
+Route::middleware(['auth', 'adminMiddleware'])->group(function () {
+    Route::get('adminDashboard', [AdminController::class, 'index'])->name('adminDashboard');
+});
